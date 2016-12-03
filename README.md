5d1a3de85af31c3ae05feb5ffb5330fa71481599
Mike Weber <mike@weberapps.com>
2016-12-03 15:57:04 -0600

    Add border and collision detection

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
-# brickbreaker
+# Brick Breaker
 Simple canvas drawing example
 
 # TODO
-* Add collision detection to edge of window
-* Add a paddle
-  * Make it moveable with the keyboard
-  * Add collision detection
-* Add bricks
-  * Draw bricks in array
-  * Add collision detection
-  * Remove damaged bricks
+* [x] Add collision detection to edge of window
+* [ ] Add a paddle
+  * [ ] Make it moveable with the keyboard
+  * [ ] Add collision detection
+* [ ] Add bricks
+  * [ ] Draw bricks in array
+  * [ ] Add collision detection
+  * [ ] Remove damaged bricks
--- bb.js
+++ bb.js
@@ -1,11 +1,12 @@
 window.BrickBreaker = (function() {
   var klass = function BrickBreaker(width, height, canvas) {
-    this.width  = width
-    this.height = height
+    this.width     = width
+    this.height    = height
     this.setCanvas(canvas)
     // 60 fps = 1 frame / 60 seconds = 0.016666 seconds = 16 milliseconds
     this.frame_length = 16
-    this.ball   = new Ball(5, { x: width / 2, y: 50 }, { x: 0, y: 0 });
+    this.ball      = new Ball(10, { x: width / 2, y: 50 }, { x: 5, y: 0 });
+    this.play_area = new PlayArea(this.width, this.height)
   }
 
   klass.prototype.setCanvas = function(canvas) {
@@ -26,14 +27,19 @@ window.BrickBreaker = (function() {
       var now = new Date(),
           dt  = now - this.last_draw
 
+      // Move the ball
+      this.moveBall(dt * 0.001)
+      // Check to see if the ball has collided with anything,
+      // and make it bounce if it has.
+      this.performCollisions(this.ball)
+
       // Wait to draw until frame_length milliseconds have passed
       if (dt > this.frame_length) {
-        // Move the ball
-        this.moveBall(dt * 0.001)
         // Clear the canvas
         this.clearCanvas()
         // Draw the ball in it's new position
         this.drawBall()
+        this.drawBorder()
         // Remember the current timestamp, so we can
         // calculate the time between frames
         this.last_draw = now
@@ -48,8 +54,28 @@ window.BrickBreaker = (function() {
     this.ball.move(dt)
   }
 
+  klass.prototype.performCollisions = function(moving_object) {
+    this.performBorderCollisions(moving_object, this.play_area)
+  }
+
+  klass.prototype.performBorderCollisions = function(ball, container) {
+    if (ball.getTop() < container.getCollisionTop()) {
+      ball.bounceDown(container.getCollisionTop())
+    } else if (ball.getBottom() > container.getCollisionBottom()) {
+      ball.bounceUp(container.getCollisionBottom())
+    } else if (ball.getLeft() < container.getCollisionLeft()) {
+      ball.bounceRight(container.getCollisionLeft())
+    } else if (ball.getRight() > container.getCollisionRight()) {
+      ball.bounceLeft(container.getCollisionRight())
+    }
+  }
+
   klass.prototype.drawBall = function() {
     this.ball.draw(this.ctx)
+  }
+
+  klass.prototype.drawBorder = function() {
+    this.play_area.draw(this.ctx)
   }
 
   klass.prototype.clearCanvas = function() {
@@ -75,14 +101,121 @@ window.Ball = (function() {
   }
 
   klass.prototype.draw = function(context) {
+    context.save()
     context.fillStyle = '#333'
     context.beginPath()
     context.arc(this.position.x, this.position.y, this.radius, 0, 2 * Math.PI)
+    context.closePath()
     context.fill()
+    context.restore()
   }
 
   klass.prototype.getGravity = function() {
     return 9.8
+  }
+
+  klass.prototype.getTop = function() {
+    return this.position.y - this.radius
+  }
+
+  klass.prototype.getBottom = function() {
+    return this.position.y + this.radius
+  }
+
+  klass.prototype.getLeft = function() {
+    return this.position.x - this.radius
+  }
+
+  klass.prototype.getRight = function() {
+    return this.position.x + this.radius
+  }
+
+  klass.prototype.bounceUp = function(surface) {
+    this.position.y = surface - this.radius
+    this.bounceYAxis()
+  }
+
+  klass.prototype.bounceDown = function(surface) {
+    this.position.y = surface + this.radius
+    this.bounceYAxis()
+  }
+
+  klass.prototype.bounceLeft = function(surface) {
+    this.position.x = surface - this.radius
+    this.bounceXAxis()
+  }
+
+  klass.prototype.bounceRight = function(surface) {
+    this.position.x = surface + this.radius
+    this.bounceXAxis()
+  }
+
+  klass.prototype.bounceXAxis = function() {
+    this.velocity.x *= -0.5
+  }
+
+  klass.prototype.bounceYAxis = function() {
+    this.velocity.x *= 0.99
+    this.velocity.y *= -0.9
+  }
+
+  return klass
+})()
+
+window.PlayArea = (function() {
+  var klass = function PlayArea(width, height) {
+    this.width        = width
+    this.height       = height
+    this.border_width = 5
+  }
+
+  klass.prototype.draw = function(context) {
+    context.save()
+    context.strokeStyle = '#533'
+    context.lineWidth   = this.border_width
+    // Start at the top left, then move clockwise around the rectangle,
+    // and draw a line connecting each corner.
+    context.beginPath()
+    context.moveTo(this.getTop(), this.getLeft())
+    context.lineTo(this.getTop(), this.getRight())
+    context.lineTo(this.getBottom(), this.getRight())
+    context.lineTo(this.getBottom(), this.getLeft())
+    context.lineTo(this.getTop(), this.getLeft())
+    context.closePath()
+    context.stroke()
+    context.restore()
+  }
+
+  klass.prototype.getCollisionTop = function() {
+    return this.getTop() + this.border_width / 2
+  }
+
+  klass.prototype.getCollisionBottom = function() {
+    return this.getBottom() - this.border_width / 2
+  }
+
+  klass.prototype.getCollisionLeft = function() {
+    return this.getLeft() + this.border_width / 2
+  }
+
+  klass.prototype.getCollisionRight = function() {
+    return this.getRight() - this.border_width / 2
+  }
+
+  klass.prototype.getTop = function() {
+    return 0
+  }
+
+  klass.prototype.getBottom = function() {
+    return this.height
+  }
+
+  klass.prototype.getLeft = function() {
+    return 0
+  }
+
+  klass.prototype.getRight = function() {
+    return this.width
   }
 
   return klass
